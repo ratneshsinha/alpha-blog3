8698f626a6e27cdfb7515e2b79bb623204a4c684
Ratnesh Sinha <ratnesh.sinha@gmail.com>
2021-11-15 10:09:09 -0600

    Initial Commit, Articles Show working

---
+++ db/migrate/20211115030842_update_articles.rb
@@ -0,0 +1,4 @@
+class UpdateArticles < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
---
+++ db/migrate/20211115031122_update2_articles.rb
@@ -0,0 +1,5 @@
+class Update2Articles < ActiveRecord::Migration[6.1]
+  def change
+    add_column :articles, :description, :text
+  end
+end
