d3dc41b02111d42897d07089c11dc1133be6894d
Ratnesh Sinha <ratnesh.sinha@gmail.com>
2021-11-15 12:48:05 -0600

    Working edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,42 @@
 class ArticlesController < ApplicationController
   def show
-    byebug
+    # byebug
     @article = Article.find(params[:id])
+  end
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    # render plain: params[:article]
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    # render plain: @article
+    # render plain: @article.inspect
+
+    if @article.save
+      flash[:notice] = "Article was created successfully"
+      # redirect_to article_path(@article)
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated sucessfully"
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
-  validates :title, presence: true, length: {minimum: 8, maximum: 10}
-  validates :description, presence: true, length: {minimum:10, maximum: 300}
+  validates :title, presence: true, length: {minimum: 5, maximum: 100}
+  validates :description, presence: true, length: {minimum:5, maximum: 300}
 end
